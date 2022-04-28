d39920812ee0d78a8710b1cc18b049135e6e2ba3
Kris Dunning <dunning.kris@gmail.com>
2022-04-28 07:20:24 -0700

    lab 6 due

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Lab04-StructuredWebPages
 <!-- https://dgiacona.github.io/Hello-World/
 
 https://docs.google.com/drawings/d/1BreRc1CJsmM0C7QLXxvDIzAZoSltJZeq9jHCa7FLfOc/edit?usp=sharing -->
+
+Attribution
+
+*****
+
+- Cat Icon by Hea Poh Lin from NounProject.com  
+- Four Tricolor Jack Russell Dogs On Two Wooden Chairs Indoor by Scopio from NounProject.com
+- Festive Scene Of Cat In A Holiday Sweater, With A Wreath, Candy Canes, Wide Composition by Anna Ivanova from NounProject.com
--- app.js
+++ app.js
@@ -1,2 +1,51 @@
 'use strict';
 
+let popup = document.getElementById("popupC");
+let pppBTNS=document.getElementById("popupB");
+let h1text=document.getElementById("h1text");
+
+window.onload = function runscript(){
+//window.addEventListener("load", () => {
+
+    // console.log("window event listener activated");
+        popup.style.display="block";
+       // pppBTNS.style.display="block";
+        // popup.setAttribute("display", "block");    
+        // pppBTNS.setAttribute("display", "block");
+    
+
+        let catBTN = document.getElementById("catsButton");
+        let dogBTN = document.getElementById("dogsButton");
+        // let ppp = document.getElementById("pppp");
+        // ppp.setAttribute("display", "block");
+        catBTN.addEventListener("click",selectCats);
+        dogBTN.addEventListener("click",selectDogs);
+   // });
+
+console.log("runscript loaded");
+}
+
+
+//window.addEventListener("load", "openPopup")
+    function selectCats(){
+        console.log("cat btn clicked");
+       // let popup = document.getElementById("popupC");
+        popup.style.display="none";
+        let catIcon= document.getElementById("headerIcon");
+        catIcon.src="images/caticon.png";   
+        //let h1text=document.getElementById("h1text");
+        console.log(h1text);
+        h1text.innerHTML=" Pet Adoptions CAAAAATS!";
+
+                
+    };
+    
+    function selectDogs(){
+        console.log("dog btn clicked");
+       // let popup = document.getElementById("popupC");
+        popup.style.display="none";
+        let dogIcon= document.getElementById("headerIcon");
+        dogIcon.src="images/dogicon.png";
+        console.log(h1text);
+        h1text.innerHTML=" Pet Adoptions DOOOOOGS!";
+    };

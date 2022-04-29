ef620c927d6d1d4a615287c8174b7f8a84e777ba
Kris Dunning <dunning.kris@gmail.com>
2022-04-28 17:45:04 -0700

    labs 7 and 8 due

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - Cat Icon by Hea Poh Lin from NounProject.com  
 - Four Tricolor Jack Russell Dogs On Two Wooden Chairs Indoor by Scopio from NounProject.com
 - Festive Scene Of Cat In A Holiday Sweater, With A Wreath, Candy Canes, Wide Composition by Anna Ivanova from NounProject.com
+- <a target="_blank" href="https://icons8.com/icon/qdQpy48X3Rjv/star">Star</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+- 
--- app.js
+++ app.js
@@ -4,82 +4,97 @@ let popup = document.getElementById("popupC");
 let pppBTNS=document.getElementById("popupB");
 let h1text=document.getElementById("h1text");
 let icon= document.getElementById("headerIcon");
+let catBTN = document.getElementById("catsButton");
+let dogBTN = document.getElementById("dogsButton");
+let ratePopup=document.getElementById("ratePopup");
+let rateRadios=document.getElementsByName("Stars");
 
-window.onload = function runscript(){
-//window.addEventListener("load", () => {
-
-    // console.log("window event listener activated");
-        popup.style.display="block";
-       // pppBTNS.style.display="block";
-        // popup.setAttribute("display", "block");    
-        // pppBTNS.setAttribute("display", "block");
-    
-
-        let catBTN = document.getElementById("catsButton");
-        let dogBTN = document.getElementById("dogsButton");
-        // let ppp = document.getElementById("pppp");
-        // ppp.setAttribute("display", "block");
-        catBTN.addEventListener("click",selectCats);
-        dogBTN.addEventListener("click",selectDogs);
-   // });
-
-console.log("runscript loaded");
+window.onload = function runscript()
+{
+    popup.style.display="block";
+    catBTN.addEventListener("click",selectCats);
+    dogBTN.addEventListener("click",selectDogs);
+    //console.log("runscript loaded");
 }
 
-
-//window.addEventListener("load", "openPopup")
-    function selectCats(){
-        console.log("cat btn clicked");
-       // let popup = document.getElementById("popupC");
-        popup.style.display="none";
-        let catIcon= document.getElementById("headerIcon");
-        catIcon.src="images/caticon.png";   
-        //let h1text=document.getElementById("h1text");
-        console.log(h1text);
-        h1text.innerHTML=" Pet Adoptions CAAAAATS!";
-        let mainImage=document.getElementById("home_main_image");
-        mainImage.src="images/christmasCat.jpg";
-        let subIMG1=document.getElementById("subimage1");
-        let subIMG2=document.getElementById("subimage2");
-        let subIMG3=document.getElementById("subimage3");
-        subIMG1.src="images/christmasCat.jpg";
-        subIMG2.src="images/christmasCat.jpg";
-        subIMG3.src="images/christmasCat.jpg";
+function selectCats()
+{
+    //console.log("cat btn clicked");
+    popup.style.display="none";
+    let catIcon= document.getElementById("headerIcon");
+    catIcon.src="images/caticon.png";   
+    // console.log(h1text);
+   // h1text.innerHTML=" Pet Adoptions CAAAAATS!";
+    let mainImage=document.getElementById("home_main_image");
+    mainImage.src="images/christmasCat.jpg";
+    let subIMG1=document.getElementById("subimage1");
+    let subIMG2=document.getElementById("subimage2");
+    let subIMG3=document.getElementById("subimage3");
+    subIMG1.src="images/christmasCat.jpg";
+    subIMG2.src="images/christmasCat.jpg";
+    subIMG3.src="images/christmasCat.jpg";
                 
-    };
+ };
     
-    function selectDogs(){
-        console.log("dog btn clicked");
-       // let popup = document.getElementById("popupC");
-        popup.style.display="none";
-        let dogIcon= document.getElementById("headerIcon");
-        dogIcon.src="images/dogicon.png";
-        console.log(h1text);
-        h1text.innerHTML=" Pet Adoptions DOOOOOGS!";
-        let mainImage=document.getElementById("home_main_image");
-        mainImage.src="images/fourDogsOnChairs.jpg";
-        let subIMG1=document.getElementById("subimage1");
-        let subIMG2=document.getElementById("subimage2");
-        let subIMG3=document.getElementById("subimage3");
-        subIMG1.src="images/fourDogsOnChairs.jpg";
-        subIMG2.src="images/fourDogsOnChairs.jpg";
-        subIMG3.src="images/fourDogsOnChairs.jpg";
-    };
+function selectDogs()
+{
+    //console.log("dog btn clicked");
+    popup.style.display="none";
+    let dogIcon= document.getElementById("headerIcon");
+    dogIcon.src="images/dogicon.png";
+    //console.log(h1text);
+    //h1text.innerHTML=" Pet Adoptions DOOOOOGS!";
+    let mainImage=document.getElementById("home_main_image");
+    mainImage.src="images/fourDogsOnChairs.jpg";
+    let subIMG1=document.getElementById("subimage1");
+    let subIMG2=document.getElementById("subimage2");
+    let subIMG3=document.getElementById("subimage3");
+    subIMG1.src="images/fourDogsOnChairs.jpg";
+    subIMG2.src="images/fourDogsOnChairs.jpg";
+    subIMG3.src="images/fourDogsOnChairs.jpg";
+};
 
 
-function iconSpin() {
+function iconSpin()
+{
+     if (icon.src.includes("cat"))
+        { 
+        h1text.innerHTML=" CAAAAATS!"
+        }
+     else if (icon.src.includes("dog"))
+     {
+         h1text.innerHTML=" DOOOOOGS!";
+     };
 
- icon.style.transform = "rotateY(180deg)";
- icon.style.transform = 'rotate(180deg)';
-    // if (deg=="180deg")
-    // {
-    //         deg="360deg";
-    // }
-    // else if (deg=="360deg")
-    // {
-    //          deg="180deg";
-    // }
-    
+    icon.style.transform = "rotateY(180deg)";
+    icon.style.transform = 'rotate(180deg)';
+    //console.log("ICOn SPIN!");
+}
 
- console.log("ICOn SPIN!");
+function showRating()
+{
+    ratePopup.style.display="flex";
+}
+
+function rateMe()
+{
+
+    for (let i= 0; i < rateRadios.length; i++)
+    {
+        if(rateRadios[i].checked)
+        {
+        if (rateRadios[i].value>4)
+        {
+            console.log("Yay! Thank you!");
+            alert("THANK YOU SO MUCH KIND STRANGER!");
+            ratePopup.style.display="none";
+            rateRadios[i].checked=false;
+        }    
+        else
+        {
+            alert("Sorry please rate 5 stars");
+            
+        }
+        }
+    }
 }
